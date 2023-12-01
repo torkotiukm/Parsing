d395b3d812dd39c0c1e83d8339a7d731e2da3a31
Maksym Torkotiuk <maksymtorkotiuk@gmail.com>
2023-12-01 13:23:45 +0100

    Add save to db

--- autoria.py
+++ autoria.py
@@ -3,7 +3,8 @@ import requests
 from bs4 import BeautifulSoup
 import sqlite3
 
-URL = "https://auto.ria.com/uk/newauto/marka-subaru/"
+URL = input("Введи посилання:")
+name_table= input("Введи назву таблиці на англ:")
 
 HEADERS = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, '
@@ -25,35 +26,57 @@ def get_content(html): #збвр данних
         items = soup.find_all("div", class_= "proposition_area") #находимо потрібні теги
         for item in items: #перебираємо теги 
             products.append({ #словник для товару 
-                "Title": item.find("span", class_= "link").text, #записуємо загтловок 
+                "Title": item.find("span", class_= "link").text[1:-1], #записуємо загтловок 
                 "USD": int(item.find("span", class_= "green bold size22").text[4:-6].replace(" ", "")), #записуємо посиланнґм на товар
-                "UAH": item.find("span", class_= "size16").text[:-4], #записуємо ціну 
+                "UAH": int(item.find(lambda tag: tag.name == 'span' and tag.get('class') == ['size16']).text[:-4].replace(" ", "")), #записуємо ціну 
                 "City": item.find("span", class_= "item region").text[:-1],
-                "Fuel": item.find(lambda tag: tag.name == 'span' and tag.get('class') == ['item']).text.replace("•", "")
+                "Fuel": item.find(lambda tag: tag.name == 'span' and tag.get('class') == ['item']).text[1:-1].replace("•", "")
             })
+        return products #повертаємо сптсок товарів
+    else:
+        print(html.status_code)
+
+def pagination(html):
+    if html.status_code == 200: #якщр сайт нам відповів
+        soup = BeautifulSoup(html.text, "lxml")
         pages = soup.find_all('span', class_='page-item mhide')
         p1 = 1
         for page in pages:
             p = int(page.text)
             if p > p1:
                 p1 = p
-        return products, p1 #повертаємо сптсок товарів
+        return p1
     else:
         print(html.status_code)
+
+def save_data(items):
+    with sqlite3.connect("autoria.db") as conn:
+        db = conn.cursor()
+        db.execute(f"""CREATE TABLE IF NOT EXISTS {name_table}(
+                   Title TEXT,
+                   USD INT,
+                   UAH INT,
+                   City TEXT,
+                   Fuel TEXT 
+        )""")
+        db.execute(f"""DELETE FROM {name_table}""")
+        for item in items:
+            db.execute(f"""INSERT INTO {name_table} VALUES(?,?,?,?,?)""", [item["Title"], item["USD"], item["UAH"], item["City"], item["Fuel"]])
+        conn.commit()
 
 
 def parse():
     html = get_html(URL)
-    prod1, pages = get_content(html)
+    prod1 = get_content(html)
     prod = list()
     prod.extend(prod1)
+    pages = pagination(html)
     print(f"Parsing 1 page from {pages} pages")
     for i in range(2,pages+1): #задаємл кільк сторінок на сайті
        html = get_html(f"https://auto.ria.com/uk/newauto/marka-subaru/?page={i}") 
        prod.extend(get_content(html))
        print(f"Parsing {i} page from {pages} pages")
-    print(prod)
-    #savedata(prod)
+    save_data(prod)
 
 
 parse()
